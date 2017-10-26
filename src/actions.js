92fff39b83f313bfb2c39b4449130e2c6cd595bd
esther <estheralvaro44@gmail.com>
2017-10-26 09:50:58 -0500

    c

--- src/actions.js
+++ src/actions.js
@@ -1,28 +1,21 @@
 import store from './store'
 
-export const playAction = () => {
-  let selectedSong = store.getState().selectedSong
-  if (selectedSong == -1 )
-     selectedSong = 0
-
-  store.setState({
-     selectedSong : selectedSong
+export const addComent = (user,moreComent) => {
+  let firstList = store.getState().date;
+  const actualList =firstList.concat({
+    id : firstList.length,
+    name : user,
+    coment : moreComent
   })
-}
-
-export const nextAction = () => {
-
-  let selectedSong = store.getState().selectedSong + 1 //es el estado inicial de mi store
-  if (selectedSong == store.getState().songs.length )//getstate obtiene el estado actual del store.obtiene y almacena el estado
-     selectedSong = 0
-
-  store.setState({//haace una notificacion al render principal de q ha hecho un cambio
-     selectedSong : selectedSong 
-  })
-}
-
-export const stopAction = () => {
   store.setState({
      selectedSong : -1
   })
+}
+
+export const deleteComent = (id) => {
+  let lists = store.getState().list;
+  lists = lists.filter(item => item.id == id);
+  store.setState({
+    list: lists
+  });
 }
