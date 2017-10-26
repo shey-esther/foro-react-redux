dc810b0e5f070ffe175854c1ca6f0e59305bf7ef
esther <estheralvaro44@gmail.com>
2017-10-26 09:50:22 -0500

    c

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import {connect} from 'redux-zero/react';
-import {playAction, nextAction, stopAction} from './actions'
+import {addComent,deleteComent} from './actions'
 import './App.css';
 
+const App = ({date}) => {
+  const onClick = e =>{
+    e.preventDefault();
+    deleteComent(e.target.id);
+  };
+  
+  const Listar = date.map((item,index) => {
+    return (
+      <li key={index}>
+        <div className="col-md-12 col-md-12">
+          <div className="col-xs-3">
+            <img src="" />
+          </div>
+          <div className="col-md-9">
+            {item.name}
+            <hr />
+            {item.text}
+          </div>
+        </div>
+        <button onClick={onClick} id={index}>delete</button>
+        <button>report</button>
+      </li>
+    );
+  });
+  const onSubmit = e => {
+    e.preventDefault();
+    let user = this.input.value;
+    let opinion = this.input1.value
+    addComent(user,opinion);
+  };
 
-const App = () => {
   return (
-    <div className="wrapper">
-      <form id="registrar" >
-        <input type="text"  name="name" placeholder="Name" /><hr />
-        <input type="text" text="text" placeholder="Write your comment" /><hr />
+    <div className="contain">
+      <form id="registrar" onSubmit={onSubmit}>
+        <input type="text" className="form-control"  id="name" placeholder="Name" ref = {e=> (this.input = e) } /><hr />
+        <input type="text" className="form-control" id="text" placeholder="Write your comment" ref = {e=> (this.input1 = e) }  /><hr />
         <button type="submit" name="submit" value="submit">Submit</button>
       </form>
       <div className="main">
-        <h2>0Comments</h2>
+        <h2>Comments</h2>
+        <ul>{Listar}</ul>
       </div>
     </div>
   );
 }
+const mapToProps = ({date}) =>({date});
 
-export default App;
+export default connect(mapToProps)(App);
